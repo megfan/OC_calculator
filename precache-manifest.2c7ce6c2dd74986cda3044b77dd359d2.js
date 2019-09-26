36d70568623b87656cd87c01f28c4216807aab82
megfan <megfa@yahoo.com>
2019-09-26 11:43:27 +0200

    Updates

---
+++ precache-manifest.2c7ce6c2dd74986cda3044b77dd359d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8091e7ab1f9c8f43ed6a236bcd4134d",
+    "url": "/megfan/OC_calculator/index.html"
+  },
+  {
+    "revision": "5f10b7bbdd6a1c0269f7",
+    "url": "/megfan/OC_calculator/static/css/main.d6f6c7dc.chunk.css"
+  },
+  {
+    "revision": "29fb09a8652db03b1170",
+    "url": "/megfan/OC_calculator/static/js/2.6bb0f596.chunk.js"
+  },
+  {
+    "revision": "5f10b7bbdd6a1c0269f7",
+    "url": "/megfan/OC_calculator/static/js/main.8cc07ea3.chunk.js"
+  },
+  {
+    "revision": "1d103d400419abc53453",
+    "url": "/megfan/OC_calculator/static/js/runtime-main.a37c82d0.js"
+  }
+]);
